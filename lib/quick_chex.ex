29506383ed22eadce0d576e10ab4bc42c398967c
Matteo Giachino <matteo.giachino@prima.it>
2016-06-10 17:35:03 +0200

    tests with real macros

--- lib/quick_chex.ex
+++ lib/quick_chex.ex
@@ -6,47 +6,31 @@ defmodule QuickChex do
       import unquote(__MODULE__)
       import QuickChex.Generators
 
+      Module.register_attribute __MODULE__, :quick_chex_properties, accumulate: true
+
       ExUnit.plural_rule("property", "properties")
+
+      @before_compile unquote(__MODULE__)
     end
   end
 
-  defmacro property_old(message, vars, opts \\ :empty, do: contents) do
-    setup_vars = if opts === :empty do
-      nil
-    else
-      vars
-    end
-    opts = if opts === :empty do
-      vars
-    else
-      opts
-    end
-    contents = Macro.escape(contents)
-    setup_vars = Macro.escape(setup_vars)
-    quote bind_quoted: [message: message, opts: opts, contents: contents, setup_vars: setup_vars] do
-      iterations = opts[:iterations] || 10
-      generators = opts[:generators]
-      Enum.map 1..iterations, fn num ->
-        name = ExUnit.Case.register_test(__ENV__, :property, message <> to_string(num), [])
-        contents = Macro.postwalk(contents, fn
-          v = {var, context, nil} ->
-            if var in Keyword.keys(generators) do
-              Keyword.get(generators, var).()
-            else
-              v
-            end
-          other -> other
-        end)
-        if is_nil(setup_vars) do
-          def unquote(name)(_) do
-            unquote(contents)
-          end
-        else
-          def unquote(name)(unquote(setup_vars)) do
-            unquote(contents)
-          end
-        end
-      end
+  @doc false
+  defmacro __before_compile__(_env) do
+    quote do
+      #IO.inspect @quick_chex_properties
+      # @quick_chex_properties
+      # |> PropertyTest.get_values(context_vars)
+      # |> Enum.reduce(1, fn iteration_values, num ->
+      #   contents = Macro.postwalk(contents, fn
+      #     {var, context, nil} -> Keyword.get(iteration_values, var)
+      #     v -> v
+      #   end)
+      #   name = ExUnit.Case.register_test(__ENV__, :property, message <> to_string(num), [])
+      #   def unquote(name)(unquote(var)) do
+      #     unquote(contents)
+      #   end
+      #   num + 1
+      # end)
     end
   end
 
@@ -97,6 +81,12 @@ defmodule QuickChex do
         end
         num + 1
       end)
+    end
+  end
+
+  defmacro iterations(number) do
+    quote do
+      @quick_chex_properties [iterations: unquote(number)]
     end
   end
 end
--- test/quick_chex_test.exs
+++ test/quick_chex_test.exs
@@ -8,16 +8,10 @@ defmodule QuickChexTest do
     {:ok, a: a}
   end
 
-  # property "the truth 2", context, generators: [test1: int, test2: int], iterations: 50 do
-  #   assert context[:a] > test2
-  # end
-
   property "the truth 3" do
     iterations 100
     generators test1: binary(1, 100), test2: binary(1, 100)
     implies String.length(test1) < String.length(test2)
-    #IO.inspect test1
-    #IO.inspect test2
     assert String.length(test1) < String.length(test2)
   end
 end
